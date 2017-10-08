6881cb9ab16272bd1dff4959bf07b6f333fcd21f
rss775 <31736804+rss775@users.noreply.github.com>
2017-10-08 04:38:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// семестровая задача часть 1 номер 1
+// семестровая задача часть 1 номер 6
 // Харисов Раиль 11-709
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите координаты клетки в фотмате, подобном 'a1'");
-            // считываем два символа координат
-            string coord = Console.ReadLine();
-            
-            // преобразуем в координаты поля от 1 до 8
-            int horisontalPos = (byte)coord[0] - 96;
-            int verticalPos = (byte)coord[1] - 48;
+            Console.WriteLine("Введите год в формате YYYY");
+            // считываем год
+            string input = Console.ReadLine();
 
-            //если сумма этих координат без остатка делится на два то поле черное
-            if (((horisontalPos + verticalPos) % 2) == 0) Console.WriteLine("BLACK");
-            else Console.WriteLine("WHITE");                       
+            // переводим год в число и записываем в переменную
+            int year = Int32.Parse(input);
+
+            // проверяем високостность года
+            // В невисокосный год данный праздник выпадает на 13 сентября, в високосный — на 12 сентября. 
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) Console.WriteLine("12/09/" + year);
+            else Console.WriteLine("13/09/" + year);
+            
         }
     }
 }
